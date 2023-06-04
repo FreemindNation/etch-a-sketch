4c3d491ca3c242bddb3aae9049a0e054706d3144
FreemindNation <freemindnation@outlook.com>
2023-06-04 09:34:29 +0100

    Edit etch-a-sketch index.html style.css

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,16 @@
 const container = document.querySelector(".container");
+container.className = "container";
+
+function createGrid(number) {
+    for(let i = 0; i < number; i++) {
+        const column = document.createElement("div");
+        column.classList.add("cell");
+        for(let j = 0; j < number; j++) {
+            const row = document.createElement("div");
+            row.classList.add("cell");
+            column.appendChild(row);
+        }
+        container.appendChild(column);
+    }
+}
+createGrid(99);
