6f3eaa3df3f9cc0044aa45845e0264cbb2a629ca
FreemindNation <freemindnation@outlook.com>
2023-06-15 22:45:11 +0100

    Edit  README.md etch-a-sketch.js index.html style.css

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This project is part of my learning while completing the Odin Project curriculum.
 The aim of the project is to create a browser version of etch-a-sketch. it will be created using HTML, javaScript and CSS.
 I will feel in more details as i progress with the project.
+
--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,19 +2,138 @@ const body = document.querySelector("body")
 const container = document.querySelector(".container");
 container.className = "container";
 
+
+const rainbow = document.querySelector("#rainbow");
+const eraser = document.querySelector("#eraser");
+const clear = document.querySelector("#clear");
+const bgdColor = document.querySelector("#bgd-color");
+
+setInterval(()=> {
+    const selectedBgdColor = bgdColor.value
+    container.style.backgroundColor = selectedBgdColor
+}, 200);
+
+const penColor = document.querySelector("#pen-color");
+
+function getRainbowColors () {
+    let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    let randomIndex = Math.floor(Math.random() * rainbowColors.length);
+    return rainbowColors[randomIndex];
+}
+
+function increaseOpacity (element, by = 0.1) {
+    element.style.opacity = Math.min(1, +element.style.opacity + by);
+}
+
+
+let flag = false;
+
+window.onmouseup = () => {
+    flag = false;
+}
+
 function createGrid(number) {
     for(let i = 0; i < number; i++) {
         const row = document.createElement("div");
         row.classList.add("cell");
+
         for(let j = 0; j < number; j++) {
             const column = document.createElement("div");
             column.classList.add("cell");
             row.appendChild(column);
+            column.addEventListener("mouseover", () => {
+                let selectedPenColor = penColor.value;
+                if(flag) {
+
+                    column.style.backgroundColor = selectedPenColor;
+                }
+           });
+           column.addEventListener("mousedown", () => {
+                let selectedPenColor = penColor.value;
+                column.style.backgroundColor = selectedPenColor;
+                flag = true;
+           });
+
+           rainbow.addEventListener("click", () => {
+            column.addEventListener("mouseover", () => {
+                let selectedPenColor = penColor.value;
+                if(flag) {
+
+                    column.style.backgroundColor = getRainbowColors();
+                }
+           });
+            column.addEventListener("mousedown", () => {
+                let selectedPenColor = penColor.value;
+                column.style.backgroundColor = getRainbowColors();
+                flag = true;
+           });
+           });
+
+           opacity.addEventListener ("click", () => {
+            column.addEventListener("mouseover", () => {
+                let selectedPenColor = penColor.value;
+                if(flag) {
+
+                    column.style.backgroundColor = selectedPenColor;
+                    increaseOpacity(column);
+                }
+           });
+           column.addEventListener("mousedown", () => {
+                let selectedPenColor = penColor.value;
+                column.style.backgroundColor = selectedPenColor;
+                increaseOpacity(column);
+                flag = true;
+           });
+            
+           });
+
+           eraser.addEventListener("click", () => {
+            column.addEventListener("mouseover", () => {
+                let selectedPenColor = penColor.value;
+                if(flag) {
+
+                    column.style.backgroundColor = "#ffffff";
+                }
+           });
+           column.addEventListener("mousedown", () => {
+                let selectedPenColor = penColor.value;
+                column.style.backgroundColor = "#ffffff";
+                flag = true;
+           });
+           })
+
         }
+        
         container.appendChild(row);
     }
 }
 
+clear.addEventListener("click", () => {
+    location.reload();
+});
 
+function chooseGridSize () {
 
-createGrid(16);
+}
+
+function clearGrid () {
+    const gridElements = Array.from(container.childNodes);
+    gridElements.forEach(element => {
+        container.removeChild(element);
+    });
+}
+
+var gridRange = document.querySelector("#grid-range");
+var gridSize = document.querySelector("#grid-size");
+gridSize.textContent = `${gridRange.value} x ${gridRange.value}`;
+
+gridRange.addEventListener("input", (event)=> {
+    gridSize.textContent = `${event.target.value} x ${event.target.value}`;
+})
+
+gridRange.addEventListener("input", (event) => {
+    clearGrid();
+    createGrid(event.target.value);
+});
+
+createGrid(50);
